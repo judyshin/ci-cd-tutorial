00db772e36c5cd2ffd0208b7f19782f42a76b051
kiwisae99 <judyshin99@gmail.com>
2024-05-09 12:37:25 +0900

    initial commit

---
+++ src/test/java/com/cicdexample/demo/DemoCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicdexample.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
